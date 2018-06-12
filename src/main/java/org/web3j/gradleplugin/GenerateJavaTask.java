1f33ddce68cfa169235e59f57be480c8980eea99
Ugo Plouviez <up@iex.ec>
2018-06-12 15:49:25 +0200

    Add dummy test

--- src/main/java/org/web3j/gradleplugin/GenerateJavaTask.java
+++ src/main/java/org/web3j/gradleplugin/GenerateJavaTask.java
@@ -15,7 +15,6 @@ import javax.script.ScriptException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Map;
--- src/main/java/org/web3j/gradleplugin/Web3jGradlePlugin.java
+++ src/main/java/org/web3j/gradleplugin/Web3jGradlePlugin.java
@@ -6,7 +6,6 @@ import org.gradle.api.Project;
 public class Web3jGradlePlugin implements Plugin<Project> {
 
     public void apply(Project project) {
-        // project.getTasks().create("generateJava", GenerateJavaTask.class, task -> task.setContractName("michel.sol"));
         project.getTasks().create("generateJava", GenerateJavaTask.class);
     }
 }
